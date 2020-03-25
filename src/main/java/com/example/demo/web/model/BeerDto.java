8d7ee9ee371c973e8d7034c888ce75e8a9eb7063
coolio1 <coolio4325@gmail.com>
2020-03-25 19:52:27 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/web/model/BeerDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.web.model;
+
+import java.util.UUID;
+
+public class BeerDto {
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+
+}
